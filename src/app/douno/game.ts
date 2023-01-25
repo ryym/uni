2c3e733b340093948cb86ea16365c7626991cae5
ryym <ryym.64@gmail.com>
2023-01-25 09:38:36 +0900

    refactor: Move game state types into shared/

--- src/app/douno/game.ts
+++ src/app/douno/game.ts
@@ -12,53 +12,9 @@ import {
   cardById,
   parseColor,
 } from "./cards";
+import { DiscardPile, GameAction, GameConfig, GameState } from "~shared/game";
 
-export type GameConfig = {
-  readonly deck: readonly string[];
-  readonly playerUids: readonly string[];
-};
-
-export type GameState = {
-  readonly turn: number;
-  readonly currentPlayerUid: string;
-  readonly clockwise: boolean;
-  readonly deckTopIdx: number;
-  readonly playerMap: {
-    readonly [uid: string]: PlayerState;
-  };
-  readonly discardPile: DiscardPile;
-  readonly lastUpdate: null | {
-    readonly playerUid: string;
-    readonly action: GameAction;
-  };
-};
-
-export type PlayerState = {
-  readonly hand: readonly number[];
-  readonly wonAt: number | null;
-};
-
-export type DiscardPile = {
-  readonly topCards: readonly string[];
-  readonly color: Color;
-  readonly attackTotal: null | number;
-};
-
-export type GameAction =
-  | {
-      readonly type: "Start";
-    }
-  | {
-      readonly type: "Pass";
-    }
-  | {
-      readonly type: "Draw";
-    }
-  | {
-      readonly type: "Play";
-      readonly cardIndice: readonly number[];
-      readonly color: string | null;
-    };
+export type { GameAction, GameConfig, GameState } from "~shared/game";
 
 export const updateGameState = (
   config: GameConfig,
