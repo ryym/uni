cc75376abaa6c20706c1d45f40051926fb77808d
ryym <ryym.64@gmail.com>
2023-01-26 09:22:25 +0900

    feat(functions): Initialize Firebase admin and access Firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,34 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import { CODE_SHARE_TEST } from "../../shared/cards";
 
+switch (process.env.NODE_ENV) {
+  case "production": {
+    break;
+  }
+
+  case undefined:
+  case "development": {
+    /* eslint-disable @typescript-eslint/no-var-requires */
+    const dotenv = require("dotenv");
+    const path = require("path");
+    /* eslint-enable @typescript-eslint/no-var-requires */
+    dotenv.config({ path: path.resolve(process.cwd(), ".env.development") });
+    break;
+  }
+
+  default: {
+    throw new Error(`unexpected NODE_ENV: ${process.env.NODE_ENV}`);
+  }
+}
+
+const app = admin.initializeApp();
 const func = functions.region("asia-northeast1").https;
 
-export const ping = func.onCall(async (data, _context) => {
+export const ping = func.onCall(async (data, ctx) => {
+  if (ctx.auth == null) {
+    return null;
+  }
   functions.logger.info("ping with data", data);
-  return { pong: Date.now(), shared: CODE_SHARE_TEST() };
+  const doc = await app.firestore().collection("misc").doc("ping").get();
+  return { pong: Date.now(), data: doc.data() };
 });
