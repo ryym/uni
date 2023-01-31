3e8bbc014ef8c50952a42b35114cef3d7254d64d
ryym <ryym.64@gmail.com>
2023-01-31 22:19:12 +0900

    fix(sahred/array): Use endExclusive as is instead of element count

--- shared/array.ts
+++ shared/array.ts
@@ -1,3 +1,3 @@
 export const range = (start: number, endExclusive: number): number[] => {
-  return [...Array(endExclusive)].map((_, i) => start + i);
+  return [...Array(endExclusive - start)].map((_, i) => start + i);
 };
