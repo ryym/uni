ea512be779c15016af42ab67ae4cde8a9d55f3bb
ryym <ryym.64@gmail.com>
2023-02-19 13:24:17 +0900

    feat(home): Enable to create room

--- src/views/home/HomePage.tsx
+++ src/views/home/HomePage.tsx
@@ -1,5 +1,39 @@
-import { ReactElement } from "react";
+import { FormEvent, ReactElement, useState } from "react";
+import { useLocation } from "wouter";
+import { useCreateRoom } from "./useCreateRoom";
 
 export function HomePage(): ReactElement {
-  return <div>home page</div>;
+  const [, navigate] = useLocation();
+  const createRoom = useCreateRoom();
+
+  const [masterPassword, setMasterPassword] = useState("");
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newRoom = await createRoom(masterPassword);
+    navigate(`/rooms/${newRoom.id}`);
+  };
+
+  return (
+    <div>
+      <h2>Join room</h2>
+      <p>Ask the room owner for the room URL.</p>
+      <hr />
+
+      <h2>Create room</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          master password
+          <input
+            type="password"
+            required
+            value={masterPassword}
+            onChange={(e) => setMasterPassword(e.target.value)}
+          />
+        </label>
+        <p>Currently this application is open for limited users.</p>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
 }
---
+++ src/views/home/useCreateRoom.ts
@@ -0,0 +1,42 @@
+import { addDoc, setDoc } from "firebase/firestore";
+import { useAtomValue } from "jotai";
+import { useCallback } from "react";
+import { roomCollectionRef, roomCreatorDocRef } from "~/backend/db";
+import { log } from "~/lib/logger";
+import { firebaseAtom } from "../_store/firebase";
+import { useSignIn } from "../_store/session";
+
+export type CreateRoom = (masterPassword: string) => Promise<NewRoomInfo>;
+
+export type NewRoomInfo = {
+  readonly id: string;
+};
+
+export const useCreateRoom = (): CreateRoom => {
+  const { db } = useAtomValue(firebaseAtom);
+  const signIn = useSignIn();
+
+  const createRoom: CreateRoom = useCallback(
+    async (masterPassword) => {
+      const user = await signIn();
+
+      log.debug("registering room creator");
+      await setDoc(roomCreatorDocRef(db, user.uid), {
+        password: masterPassword,
+        registeredAt: Date.now(),
+      });
+
+      log.debug("creating room");
+      const roomRef = await addDoc(roomCollectionRef(db), {
+        createdAt: Date.now(),
+        ownerUid: user.uid,
+        members: {},
+      });
+
+      return { id: roomRef.id };
+    },
+    [db, signIn],
+  );
+
+  return createRoom;
+};
