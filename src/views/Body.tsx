d8e26263a8b0b8dc8226c08cfd2a5e102aa20aec
ryym <ryym.64@gmail.com>
2023-02-26 21:36:30 +0900

    feat: Delay any page rendering until session state is fixed
    to simplify session management in each page

--- src/views/Body.tsx
+++ src/views/Body.tsx
@@ -1,11 +1,16 @@
+import { useAtomValue } from "jotai";
 import { ReactElement } from "react";
 import { Route, Switch } from "wouter";
-import { useAuthStateSubscription } from "./_store/session";
+import { sessionAtom, useAuthStateSubscription } from "./_store/session";
 import { HomePage } from "./home/HomePage";
 import { RoomPage } from "./rooms/RoomPage";
 
 export function Body(): ReactElement {
   useAuthStateSubscription();
+  const session = useAtomValue(sessionAtom);
+  if (!session.signedIn && session.restoring) {
+    return <div>loading...</div>;
+  }
   return (
     <div>
       <h1>DoUno</h1>
