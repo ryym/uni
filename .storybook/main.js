c8895efbb2bb9517dc3270a34aece578300e6431
ryym <ryym.64@gmail.com>
2023-03-01 22:11:03 +0900

    chore(storybook): Upgrade to 7.0.0-beta
    v6 は色々問題ありすぎ。
    
    - 古い依存が多くて npm audit の警告がうるさい
    - なぜか @storybook/react (の推移的依存のどこか?) が @types/webpack を
      devDep でなく dep に指定してて node_modules に入ってくる。
        - tapable v1 系に依存してる。 happy-css-modules が tapable v2 に依存してて型の解決が上手くいかずコンパイルエラー。 skipLibCheck すれば済むけど。
    
    migration guide:
    - https://github.com/storybookjs/storybook/blob/9783bb50b200a605d713678d5d1fe795169721f1/MIGRATION.md
    - https://storybook.js.org/blog/component-story-format-3-0/

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,13 @@
 /* eslint-env node */
 
-const path = require("path");
-
-const rootDir = path.resolve(__dirname, "..");
-
-module.exports = {
+/**
+ * @type import("@storybook/react-vite").StorybookConfig
+ */
+export default {
   stories: ["../src/**/*.stories.tsx"],
   addons: ["@storybook/addon-essentials"],
-  framework: "@storybook/react",
-  features: {
-    storyStoreV7: true,
-  },
-  webpackFinal: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "~": path.join(rootDir, "src"),
-      "~shared": path.join(rootDir, "shared"),
-    };
-    return config;
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
 };
