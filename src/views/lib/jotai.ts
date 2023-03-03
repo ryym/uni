38f9bf648f1ccf453699627cd4c354b25749f06f
ryym <ryym.64@gmail.com>
2023-03-03 09:13:54 +0900

    chore(jotai): Update outdated document for pair()

--- src/views/lib/jotai.ts
+++ src/views/lib/jotai.ts
@@ -6,9 +6,9 @@ export type AnyWritableAtom<T = any> = WritableAtom<T, any[], any>;
 export type AtomInitializer<T> = readonly [AnyWritableAtom<T>, T];
 
 /**
- * A helper function to construct initialValues for Provider in typesafe manner.
- *     initialValues={[[numberAtom, ""]]} //=> compile
- *     initialValues={[pair(numberAtom, "")]} //=> compile error
+ * A helper function to construct initialValues for useHydrateAtoms in typesafe manner.
+ *     [[numberAtom, ""]] //=> compile
+ *     [pair(numberAtom, "")] //=> compile error
  */
 export const pair = <T>(atom: AnyWritableAtom<T>, initialValue: T): AtomInitializer<T> => {
   return [atom, initialValue];
