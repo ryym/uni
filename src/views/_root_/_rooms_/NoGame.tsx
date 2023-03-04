3a1f4dd9001ce8ca85a0d8299fefef993ba77fac
ryym <ryym.64@gmail.com>
2023-03-04 16:59:55 +0900

    feat(room): Add styles to room when no game begin yet

--- src/views/_root_/_rooms_/NoGame.tsx
+++ src/views/_root_/_rooms_/NoGame.tsx
@@ -1,5 +1,8 @@
-import { ReactElement } from "react";
+import { useAtomValue } from "jotai";
+import { Fragment, ReactElement } from "react";
+import { userAtom } from "~/views/store/session";
 import { RoomState } from "~shared/room";
+import styles from "./styles/NoGame.module.css";
 
 export type NoGameProps = {
   readonly room: RoomState;
@@ -7,16 +10,31 @@ export type NoGameProps = {
 };
 
 export function NoGame(props: NoGameProps): ReactElement {
+  const user = useAtomValue(userAtom);
   return (
-    <div>
-      <div>no game</div>
-      <button onClick={props.onStartGame}>Start Game</button>
-      <hr />
-      <div>members</div>
+    <div className={styles.root}>
       <div>
-        {Object.values(props.room.members)
-          .map((m) => m.name)
-          .join(", ")}
+        <p>ゲームはまだ始まっていません。</p>
+        <button onClick={props.onStartGame}>ゲームを始める</button>
+      </div>
+      <div>
+        <div className={styles.head}>メンバー</div>
+        <div className={styles.members}>
+          {Object.entries(props.room.members).map(([uid, m]) => {
+            const isOwner = uid === props.room.ownerUid;
+            return (
+              <Fragment key={uid}>
+                <div className={`${styles.memberType} ${isOwner ? styles.isOwner : ""}`}>
+                  {isOwner ? "オーナー" : "メンバー"}
+                </div>
+                <div>
+                  {m.name}
+                  <span className={styles.isYou}>{uid === user.uid ? "(You)" : ""}</span>
+                </div>
+              </Fragment>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
