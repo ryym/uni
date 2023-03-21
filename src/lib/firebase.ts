6ead6c57610d840f0c62c1b110e9ce15bb8729d4
ryym <ryym.64@gmail.com>
2023-03-21 09:18:23 +0900

    chore(env): Remove actual Firebase API key from Git
    原理的には公開しても問題ない値のはずだが、好きに API 呼べちゃうのはやはり微妙なので止める。 key は regenerate 済み。

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,9 +16,7 @@ export const buildFirebaseClient = async (): Promise<FirebaseClient> => {
     authDomain: getEnv("VITE_FIREBASE_AUTH_DOMAIN"),
     projectId: getEnv("VITE_FIREBASE_PROJECT_ID"),
     storageBucket: getEnv("VITE_FIREBASE_STORAGE_BUCKET"),
-    messagingSenderId: getEnv("VITE_FIREBASE_MESSAGING_SENDER_ID"),
     appId: getEnv("VITE_FIREBASE_APP_ID"),
-    measurementId: getEnv("VITE_FIREBASE_MEASUREMENT_ID"),
   });
   const auth = getAuth(app);
   const db = getFirestore(app);
