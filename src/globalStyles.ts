779fe0a62739dd9cb9bd53af24057eca3f372b73
ryym <ryym.64@gmail.com>
2023-07-16 10:45:02 +0900

    refactor(styles): Freeze color classes object

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,8 +1,8 @@
 import { Color } from "~shared/cards";
 
-export const cardColorClasses = {
+export const cardColorClasses = Object.freeze({
   Red: "cardRed",
   Blue: "cardBlue",
   Green: "cardGreen",
   Yellow: "cardYellow",
-} satisfies Record<Color, string>;
+}) satisfies Record<Color, string>;
